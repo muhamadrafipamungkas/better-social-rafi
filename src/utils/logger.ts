249019e2b5d87af74caf8b5fded3065a98569f9c
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-08-23 23:22:13 +0700

    📝 Setup logger utility with winston

---
+++ src/utils/logger.ts
@@ -0,0 +1,32 @@
+import config from 'configs/config'
+import winston from 'winston'
+
+interface LoggingInfo {
+  level: string
+  message: string
+}
+
+const enumerateErrorFormat = winston.format((info: LoggingInfo) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack })
+  }
+
+  return info
+})
+
+const logger = winston.createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf((info: LoggingInfo) => `${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error']
+    })
+  ]
+})
+
+export default logger
